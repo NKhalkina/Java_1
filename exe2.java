cb2a0482cbcd979f8d7d8596c7a77b9297694fa2
Natasha Khalkina <tashik2010@mail.ru>
2023-03-09 13:36:24 +0300

    Решение задачи 2

--- exe2.java
+++ exe2.java
@@ -2,13 +2,10 @@ package HomeWork_1;
 
 /*Вывести все простые числа от 1 до 1000*/
 
-import java.util.Scanner;
+
 public class exe2 {
     public static void main(String [] args) {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.print("Введите число:  ");
-        int number = iScanner.nextInt();
-        for(int i = 2; i < number; ++i){
+        for(int i = 2; i < 1000; ++i){
             int count = 0;
             for(int j = 2; j <=i && count < 2;++j){
               if(i%j==0){
